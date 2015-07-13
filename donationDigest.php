c398d2f41dcc1c38c8310c27ce83b4594154d010
zaphod1984 <zaphod84@gmx.de>
2015-07-13 20:49:31 +0200

    thank you page

--- donationDigest.php
+++ donationDigest.php
@@ -1,40 +1,42 @@
 <?php
 
-error_reporting(E_ALL);
+    error_reporting(E_ALL);
 
-require_once('./classes.php');
+    require_once('./classes.php');
 
-$payKind = $_POST['payKind'];
+    $payKind = $_POST['payKind'];
 
-if ($payKind == 'SEPA') {
-    sendMail($_REQUEST);
-} else if ($payKind == 'SOU') {
-    runExternal(new SofortDestination());
-} else if ($payKind == 'PP') {
-    runExternal(new PPDestination());
-}
-
-echo "Vielen Dank für Ihren Einsatz!";
-
-function runExternal(Destination $destination) {
-    $destination->setAmount($_POST['amount']);
-    $destination->setPurpose($_POST['purpose']);
-
-    $url = $destination->getUrl();
-    $params = http_build_query($destination->getParams());
-    $completeUrl = $url . '?' . $params;
-
-    header('Location: ' . $completeUrl);
-}
-
-function sendMail(Array $data) {
-    $text = '';
-
-    foreach ($data as $key => $value) {
-        $text .= $key . ": \n";
-        $text .= $value;
-        $text .= "\n\n";
+    if ($payKind == 'SEPA') {
+        sendMail($_REQUEST);
+        header("Location: donationThankyou.html");
+    } else if ($payKind == 'SOU') {
+        runExternal(new SofortDestination());
+        sendMail($_REQUEST);
+    } else if ($payKind == 'PP') {
+        runExternal(new PPDestination());
+        sendMail($_REQUEST);
     }
 
-    mail('f.ernst@nehemia.org', 'spende', $text);
-}
+    function runExternal(Destination $destination) {
+        $destination->setAmount($_POST['amount']);
+        $destination->setPurpose($_POST['purpose']);
+
+        $url = $destination->getUrl();
+        $params = http_build_query($destination->getParams());
+        $completeUrl = $url . '?' . $params;
+
+        header('Location: ' . $completeUrl);
+    }
+
+    function sendMail(Array $data) {
+        $text = '';
+
+        foreach ($data as $key => $value) {
+            $text .= $key . ": \n";
+            $text .= $value;
+            $text .= "\n\n";
+        }
+
+        mail('f.ernst@nehemia.org', 'spende', $text);
+    }
+?>
